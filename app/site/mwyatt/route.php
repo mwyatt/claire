4206387237e1b3ea4f3d484ab0aa37645513ec26
mwyatt <martin,wyatt@gmail.com>
2014-12-01 08:29:00 +0000

    mwyatt setup

---
+++ app/site/mwyatt/route.php
@@ -0,0 +1,6 @@
+<?php
+
+$routes->add('home', new Symfony\Component\Routing\Route(
+	'/',
+	['controller' => 'OriginalAppName\\Site\\Elttl\\Controller\\Index']
+));
---
+++ app/site/mwyatt/service/common.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OriginalAppName\Site\Mwyatt\Service;
+
+use OriginalAppName\Json;
+use OriginalAppName\Google\Analytics;
+
+/**
+ * services group up controller commands
+ * making the controllers more readable and tidy
+ */
+class Common extends \OriginalAppName\Service
+{
+
+
+	public function read()
+	{
+		
+		// menu primary
+		$json = new Json();
+		$json->read('menu-primary');
+		$menuPrimary = $json->getData();
+
+		// menu Secondary
+		$json = new Json();
+		$json->read('menu-secondary');
+		$menuSecondary = $json->getData();
+
+		// menu Tertiary
+		$json = new Json();
+		$json->read('menu-tertiary');
+		$menuTertiary = $json->getData();
+
+		// divisions
+		$modelTennisDivision = new ModelTennis\Division();
+		$modelTennisDivision->read();
+
+		// template
+		return [
+			'googleAnalyticsTrackingId' => 'UA-35261063-1',
+			'year' => 0,
+			'divisions' => $modelTennisDivision->getData(),
+			'menuPrimary' => $menuPrimary,
+			'menuSecondary' => $menuSecondary,
+			'campaign' => new Analytics\Campaign(),
+			'menuTertiary' => $menuTertiary
+		];
+	}
+}
