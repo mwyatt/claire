13a35c7fbbabc8d8b3cd9c3b263699f43d2118a7
Martin Wyatt <martin.wyatt@gmail.com>
2014-12-16 08:30:28 +0000

    table definitions

--- app/sql/table.sql
+++ app/sql/table.sql
@@ -1,10 +1,85 @@
-CREATE TABLE `shop_stock_reminder` (
-	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`product_id` INT(11) UNSIGNED NULL DEFAULT '0' COMMENT 'product which customer needs reminding about',
-	`email` VARCHAR(254) NULL DEFAULT '' COMMENT 'customers email address',
-	`time_created` INT(11) NOT NULL DEFAULT '0' COMMENT 'epoch of when the row was created',
-	PRIMARY KEY (`id`)
-)
-COMMENT='holds a list of email addresses which will be periodically removed using a cron task when the product becomes in stock.'
-COLLATE='latin1_swedish_ci'
-ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `options` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(500) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `content` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `slug` varchar(255) NOT NULL,
+  `html` longtext,
+  `type` varchar(50) NOT NULL DEFAULT '',
+  `timePublished` int(10) unsigned DEFAULT '0',
+  `status` varchar(50) NOT NULL DEFAULT 'hidden',
+  `userId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`userId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `content_meta` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `contentId` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `content_id` (`contentId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `group` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(8000) NOT NULL,
+  `time` int(12) NOT NULL,
+  `userId` int(12) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `log_admin_unseen` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `logId` int(10) NOT NULL,
+  `userId` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `media` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(500) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `path` varchar(500) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `timePublished` int(10) unsigned DEFAULT '0',
+  `userId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`userId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `timeRegistered` int(10) unsigned DEFAULT NULL,
+  `level` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `nameFirst` varchar(75) NOT NULL DEFAULT '',
+  `nameLast` varchar(75) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
