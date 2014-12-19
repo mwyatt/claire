55216d894c7cfa5592afb8bcac9bc56d62e19df8
mwyatt <martin.wyatt@gmail.com>
2014-12-19 17:16:12 +0000

    more mail processing functions

--- app/class/mail.php
+++ app/class/mail.php
@@ -19,6 +19,12 @@ class Mail extends \OriginalAppName\View
 	protected $swiftMailer;
 
 
+	protected $css;
+
+
+	protected $tag = '{example-tag}';
+
+
 	/**
 	 * @return object 
 	 */
@@ -48,6 +54,9 @@ class Mail extends \OriginalAppName\View
 		// pallete
 		$mailPallete = new \OriginalAppName\Mail\Pallete;
 		$this->view->setDataKey('mail\styles', $mailPallete);
+
+		$this->set_css('asset/screen-email.css');
+
 	}
 
 /*
@@ -113,5 +122,104 @@ class Mail extends \OriginalAppName\View
 
 		// positive
 		return true;
+	}
+
+
+	/**
+	 * grabs css file contents and stores as array
+	 * @param string $path path to css file
+	 */
+	public function setCss($path)
+	{
+		$css = file_get_contents(BASE_PATH . '/' . $path);
+		$cssSelectors = explode('}', $css);
+		$symbolOpeningCurly = '{';
+		$symbolDot = '.';
+		$finalCss = [];
+		foreach ($cssSelectors as $cssSelector) {
+
+			// must contain a selector
+			// or be a comment
+			if (! strpos($cssSelector, $symbolOpeningCurly) || strpos($cssSelector, '@')) {
+				continue;
+			}
+
+			// get '.foo-bar {' process to become 'foo-bar'
+			$cssParts = explode($symbolOpeningCurly, $cssSelector);
+			$selectorArea = current($cssParts);
+			$positionDot = strrpos($selectorArea, $symbolDot);
+			$positionCurly = strrpos($selectorArea, $symbolOpeningCurly);
+			$selectorLength = $positionCurly - $positionDot;
+			$selectorName = substr($selectorArea, $positionDot);
+			$selectorName = trim(str_replace($symbolDot, '', $selectorName));
+			
+			// get css for this selector
+			$selectorContent = substr($cssSelector, strpos($cssSelector, $symbolOpeningCurly));
+			$selectorContent = trim(str_replace($symbolOpeningCurly, '', $selectorContent));
+
+			// store as [foo-bar] = 'padding: 0;'
+			$finalCss[$selectorName] = $selectorContent;
+		}
+		$this->css = $finalCss;
+	}
+
+
+	/**
+	 * goes through all styles and replaces any found {$key} with
+	 * its appropriate css
+	 */
+	public function tagReplaceCss()
+	{
+		$body = $this->get_body();
+		foreach ($this->getCss() as $key => $css) {
+			$match = '{' . $key . '}';
+			if (strpos($body, $match) === false) {
+				continue;
+			}
+			$body = str_replace($match, $css, $body);
+		}
+		$this->set_body($body);
+		return $this;
+	}
+
+
+	/**
+	 * random html being used in emails, attempts to style
+	 * all supported elements {foo-bar}
+	 * @param  string $html 
+	 * @return string       html with {keys}
+	 */
+	public function tagInject($html)
+	{
+
+		// supported elements
+		// can this be worked out from pool of classes? yes.
+		foreach ($this->getCss() as $selector => $properties) {
+			$tag = '{' . $selector . '}';
+			$find = '<' . $selector;
+			$replace = $find . ' style="' . $tag . '"';
+			if (strpos($html, $find) === false) {
+				continue;
+			}
+			$html = str_replace($find, $replace, $html);
+		}
+		return $html;
+	}
+
+
+	public function getCss()
+	{
+		return $this->css;
+	}
+
+
+	/**
+	 * get css selector
+	 * @param  string $key foo-bar
+	 * @return string      css properties
+	 */
+	public function getCssKey($key)
+	{
+		return isset($this->css[$key]) ? $this->css[$key] : '';
 	}
 }
