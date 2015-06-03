a4c175a374eec74a97e2c5d542672f70fb4610e3
mwyatt <martin.wyatt@gmail.com>
2015-06-03 17:28:47 +0100

    mwyatt config

---
+++ App/Site/Mwyatt/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admin/menu' => [
+        ['name' => 'Pages', 'url' => 'page/'],
+        ['name' => 'Users', 'url' => 'user/'],
+        ['name' => 'Settings', 'url' => 'settings/']
+    ]
+];
