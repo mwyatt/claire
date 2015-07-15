62429c271f91e97413aa41086200c79e4eeb02ec
mwyatt <martin.wyatt@gmail.com>
2015-07-15 16:51:24 +0100

    composer hook

--- App/Class/Controller.php
+++ App/Class/Controller.php
@@ -7,14 +7,13 @@ use OriginalAppName\Service;
 use OriginalAppName\Registry;
 use OriginalAppName\Response;
 
-/*
+/**
  * @author Martin Wyatt <martin.wyatt@gmail.com>
  * @version     0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  */
 class Controller
 {
-
 
     public $view;
 
