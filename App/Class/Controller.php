fbda406c0c7deeca0400f704f6b940712fcd7c22
mwyatt <martin.wyatt@gmail.com>
2015-07-15 16:35:26 +0100

    composer hook

--- App/Class/Controller.php
+++ App/Class/Controller.php
@@ -1,4 +1,4 @@
-<?php   
+<?php
 
 namespace OriginalAppName;
 
@@ -7,102 +7,104 @@ use OriginalAppName\Service;
 use OriginalAppName\Registry;
 use OriginalAppName\Response;
 
-
 /**
- * @author Martin Wyatt <martin.wyatt@gmail.com> 
- * @version	0.1
+ * @author Martin Wyatt <martin.wyatt@gmail.com>
+ * @version     0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  */
 class Controller
 {
 
 
-	public $view;
+    public $view;
 
 
-	public $url;
+    public $url;
 
 
-	public function __construct()
-	{
-		$registry = Registry::getInstance();
-		$this
-			->setUrl($registry->get('url'))
-			->setView(new View);
-	}
+    public function __construct()  {
+        $registry = Registry::getInstance();
+        $this
+            ->setUrl($registry->get('url'))
+            ->setView(new View);
+    }
 
 
-	public function error500($exception)
-	{
-		$this->view->setDataKey('exceptionMessage', $exception->getMessage());
-		return new Response($this->view->getTemplate('500'), 500);
-	}
+    public function error500($exception )
+    {
+        $this->view->setDataKey('exceptionMessage', $exception->getMessage());
+        return new Response($this->view->getTemplate('500'), 500);
+    }
 
 
-	/**
-	 * @return object 
-	 */
-	public function getView() {
-	    return $this->view;
-	}
-	
-	
-	/**
-	 * @param object $view 
-	 */
-	public function setView($view) {
-	    $this->view = $view;
-	    return $this;
-	}
+    /**
+     * @return object
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+    
+    
+    /**
+     * @param object $view
+     */
+    public function setView($view)
+    {
+        $this->view = $view;
+        return $this;
+    }
 
 
-	/**
-	 * redirects the user to another url and terminates
-	 * utilising the generator from symfony
-	 * @param  string $key      routeKey
-	 * @param  array $config if required
-	 * @return null             
-	 */
-	public function redirect($key, $config = [])
-	{
+    /**
+     * redirects the user to another url and terminates
+     * utilising the generator from symfony
+     * @param  string $key      routeKey
+     * @param  array $config if required
+     * @return null
+     */
+    public function redirect($key, $config = [])
+    {
 
-		// get url from registry and generate string
-		$registry = Registry::getInstance();
-		$url = $registry->get('url');
-		$url = $url->generate($key, $config);
+        // get url from registry and generate string
+        $registry = Registry::getInstance();
+        $url = $registry->get('url');
+        $url = $url->generate($key, $config);
 
-		// redirect
-		header('location:' . $url);
+        // redirect
+        header('location:' . $url);
 
-		// prevent continuation
-		exit;
-	}
-
-
-	public function redirectAbsolute($url)
-	{
-
-		// route
-		header('location:' . $url);
-
-		// prevent continuation
-		exit;
-	}
+        // prevent continuation
+        exit;
+    }
 
 
-	/**
-	 * @return object 
-	 */
-	public function getUrl() {
-	    return $this->url;
-	}
-	
-	
-	/**
-	 * @param object $url 
-	 */
-	public function setUrl($url) {
-	    $this->url = $url;
-	    return $this;
-	}
+    public function redirectAbsolute($url)
+    {
+
+        // route
+        header('location:' . $url);
+
+        // prevent continuation
+        exit;
+    }
+
+
+    /**
+     * @return object
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+    
+    
+    /**
+     * @param object $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
 }
