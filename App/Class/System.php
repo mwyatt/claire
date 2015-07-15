30b98e66946b59c4fe69ca17a11e86d319a800d6
mwyatt <martin.wyatt@gmail.com>
2015-07-15 17:29:35 +0100

    system will be removed

--- App/Class/System.php
+++ App/Class/System.php
@@ -2,52 +2,51 @@
 
 namespace OriginalAppName;
 
-
 /**
  * can remove this?
  * boilerplate for all objects in the app. responsible for how each object
  * should be implemented
- * @author Martin Wyatt <martin.wyatt@gmail.com> 
- * @version	0.1
+ * @author Martin Wyatt <martin.wyatt@gmail.com>
+ * @version     0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
- */ 
+ */
 class System
 {
 
 
-	/**
-	 * the database instance
-	 * @var object
-	 */
-	public $database;
+    /**
+     * the database instance
+     * @var object
+     */
+    public $database;
 
 
-	/**
-	 * @param object $value 
-	 */
-	public function setDatabase($value)
-	{
-		$this->database = $value;
-	}
+    /**
+     * @param object $value
+     */
+    public function setDatabase($value)
+    {
+        $this->database = $value;
+    }
 
 
-	/**
-	 * @param object $value 
-	 */
-	public function getDatabase()
-	{
-		return $this->database;
-	}
+    /**
+     * @param object $value
+     */
+    public function getDatabase()
+    {
+        return $this->database;
+    }
 
 
-	/**
-	 * debug mode is set
-	 * @return boolean true if debug equals this class name
-	 */
-	public function isDebug($theObject)
-	{
-		if (array_key_exists('debug', $_REQUEST) && strtolower($_REQUEST['debug']) == strtolower(get_class($theObject))) {
-			return true;
-		}
-	}
+    /**
+     * debug mode is set
+     * @return boolean true if debug equals this class name
+     */
+    public function isDebug($theObject)
+    {
+        if (array_key_exists('debug', $_REQUEST) && strtolower($_REQUEST['debug']) == strtolower(get_class($theObject))) {
+            return true;
+        }
+    }
 }
