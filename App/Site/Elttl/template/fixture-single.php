cf4bd2b6545147123b133d14bd44d9c1d731454e
mwyatt <martin.wyatt@gmail.com>
2015-07-28 10:58:47 +0100

    player/single, fixture/single

--- App/Site/Elttl/template/fixture-single.php
+++ App/Site/Elttl/template/fixture-single.php
@@ -4,15 +4,15 @@
 	<p class="fixture-single-time">Fulfilled on <?php echo date('D jS F Y', $fixture->timeFulfilled); ?></p>
 	<h1 class="page-primary-title"><?php echo $teamLeft->name ?> vs <?php echo $teamRight->name ?></h1>
 	<!-- <p><?php echo $division->name ?> division</p> -->
-	<table class="elttl-table is-encounter">
+    <table class="elttl-table is-encounter">
 
 <?php include $this->getTemplatePath('_encounters') ?>
 
-		<tr class="elttl-table-row">
+        <tr class="elttl-table-row">
 			<td class="elttl-table-cell is-score-total" colspan="2"><?php echo $fixtureResult->scoreLeft ?></td>
 			<td class="elttl-table-cell is-score-total" colspan="2"><?php echo $fixtureResult->scoreRight ?></td>
-		</tr>
-	</table>
+        </tr>
+    </table>
 </div>
 
 <?php include $this->getTemplatePath('_footer') ?>
