610ddf33ee80a6102071e097a17ab61a1301d497
mwyatt <martin.wyatt@gmail.com>
2015-07-31 08:06:52 +0100

    elttl/service/common

--- App/Site/Elttl/Service/Common.php
+++ App/Site/Elttl/Service/Common.php
@@ -14,36 +14,36 @@ class Common extends \OriginalAppName\Service
 {
 
 
-	public function read()
-	{
-		
-		// menu primary
-		$json = new Json();
-		$json->read('menu-primary');
-		$menuPrimary = $json->getData();
+    public function read()
+    {
+        
+        // menu primary
+        $json = new Json();
+        $json->read('menu-primary');
+        $menuPrimary = $json->getData();
 
-		// menu Secondary
-		$json = new Json();
-		$json->read('menu-secondary');
-		$menuSecondary = $json->getData();
+        // menu Secondary
+        $json = new Json();
+        $json->read('menu-secondary');
+        $menuSecondary = $json->getData();
 
-		// menu Tertiary
-		$json = new Json();
-		$json->read('menu-tertiary');
-		$menuTertiary = $json->getData();
+        // menu Tertiary
+        $json = new Json();
+        $json->read('menu-tertiary');
+        $menuTertiary = $json->getData();
 
-		// divisions
-		$modelTennisDivision = new ModelTennis\Division();
-		$modelTennisDivision->readYearId();
+        // divisions
+        $modelTennisDivision = new ModelTennis\Division();
+        $modelTennisDivision->readYearId();
 
-		// template
-		return [
-			'googleAnalyticsTrackingId' => 'UA-35261063-1',
-			'divisions' => $modelTennisDivision->getData(),
-			'menuPrimary' => $menuPrimary,
-			'menuSecondary' => $menuSecondary,
-			'campaign' => new Analytics\Campaign(),
-			'menuTertiary' => $menuTertiary
-		];
-	}
+        // template
+        return [
+            'googleAnalyticsTrackingId' => 'UA-35261063-1',
+            'divisions' => $modelTennisDivision->getData(),
+            'menuPrimary' => $menuPrimary,
+            'menuSecondary' => $menuSecondary,
+            'campaign' => new Analytics\Campaign(),
+            'menuTertiary' => $menuTertiary
+        ];
+    }
 }
