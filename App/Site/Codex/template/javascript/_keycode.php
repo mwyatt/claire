3b0a0fcd8552c06bab742253ad0dd6a2f1debdab
mwyatt <martin.wyatt@gmail.com>
2015-08-11 12:55:10 +0100

    codex js, gulp packages

--- App/Site/Codex/template/javascript/_keycode.php
+++ App/Site/Codex/template/javascript/_keycode.php
@@ -1,6 +1,6 @@
 <?php $html = file_get_contents(SITE_PATH . 'js/keyCode.js') ?>
 
 <div class="typography">
-	<p>Allows human readable translation for the way javascript interprets keypresses.</p>
+    <p>Allows human readable translation for the way javascript interprets keypresses.</p>
 </div>
 <pre class="rainbow-pre"><code data-language="js"><?php echo htmlentities($html) ?></code></pre>
