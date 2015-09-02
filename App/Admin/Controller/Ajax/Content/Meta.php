be4810944637b9c393e5cf433bf82924c6b96ed1
mwyatt <martin.wyatt@gmail.com>
2015-09-02 17:19:41 +0100

    model update, delete and remove old code

--- App/Admin/Controller/Ajax/Content/Meta.php
+++ App/Admin/Controller/Ajax/Content/Meta.php
@@ -9,105 +9,103 @@ use OriginalAppName\Session;
 use OriginalAppName\Model;
 use OriginalAppName\Response;
 
-
-
 /**
  * untested
- * @author Martin Wyatt <martin.wyatt@gmail.com> 
- * @version	0.1
+ * @author Martin Wyatt <martin.wyatt@gmail.com>
+ * @version     0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  */
 class Meta extends \OriginalAppName\Admin\Controller\Ajax
 {
 
 
-	public function all()
-	{
+    public function all()
+    {
 
-		// validate
-		if (! isset($_GET['contentId'])) {
-			throw new Exception;
-		}
+        // validate
+        if (! isset($_GET['contentId'])) {
+            throw new Exception;
+        }
 
-		// resource
-		$modelContentMeta = new Model\Content\Meta;
+        // resource
+        $modelContentMeta = new Model\Content\Meta;
 
-		// action
-		$modelContentMeta->readColumn('contentId', $_GET['contentId']);
+        // action
+        $modelContentMeta->readColumn('contentId', $_GET['contentId']);
 
-		// output
-		return new Response(json_encode($modelContentMeta->getData()));
-	}
-
-
-	public function create()
-	{
-
-		// validate
-		if (! isset($_GET['contentId']) || ! isset($_GET['name']) || ! isset($_GET['value'])) {
-			throw new Exception;
-		}
-
-		// resource
-		$modelContentMeta = new Model\Content\Meta;
-		$entityContentMeta = new Entity\Content\Meta;
-
-		// action
-		$entityContentMeta->consumeArray($_GET);
-		$modelContentMeta
-			->create([$entityContentMeta])
-			->readId($modelContentMeta->getLastInsertIds());
-
-		// output
-		return new Response(json_encode($modelContentMeta->getDataFirst()));
-	}
+        // output
+        return new Response(json_encode($modelContentMeta->getData()));
+    }
 
 
-	public function delete()
-	{
+    public function create()
+    {
 
-		// validate
-		if (! isset($_GET['id'])) {
-			throw new Exception;
-		}
+        // validate
+        if (! isset($_GET['contentId']) || ! isset($_GET['name']) || ! isset($_GET['value'])) {
+            throw new Exception;
+        }
 
-		// resource
-		$modelContentMeta = new Model\Content\Meta;
-		$entitiesContentMeta = $modelContentMeta
-			->readId([$_GET['id']])
-			->getData();
+        // resource
+        $modelContentMeta = new Model\Content\Meta;
+        $entityContentMeta = new Entity\Content\Meta;
 
-		// action
-		$modelContentMeta->delete($entitiesContentMeta);
+        // action
+        $entityContentMeta->consumeArray($_GET);
+        $modelContentMeta
+            ->create([$entityContentMeta])
+            ->readId($modelContentMeta->getLastInsertIds());
 
-		// output
-		return new Response(json_encode($modelContentMeta->getRowCount()));
-	}
+        // output
+        return new Response(json_encode($modelContentMeta->getDataFirst()));
+    }
 
 
-	public function update()
-	{
-		
-		// validate
-		if (! isset($_REQUEST['id']) || ! isset($_REQUEST['contentId']) || ! isset($_REQUEST['name']) || ! isset($_REQUEST['value'])) {
-			throw new Exception;
-		}
+    public function delete()
+    {
 
-		// resource
-		$modelContentMeta = new Model\Content\Meta;
-		$entityContentMeta = new Entity\Content\Meta;
+        // validate
+        if (! isset($_GET['id'])) {
+            throw new Exception;
+        }
 
-		// action
-		$entityContentMeta->consumeArray($_REQUEST);
-		$modelContentMeta->update([$entityContentMeta]);
+        // resource
+        $modelContentMeta = new Model\Content\Meta;
+        $entitiesContentMeta = $modelContentMeta
+            ->readId([$_GET['id']])
+            ->getData();
 
-		// validate it happend
-		if (! $modelContentMeta->getRowCount()) {
-			throw new Exception;
-		}
+        // action
+        $modelContentMeta->delete($entitiesContentMeta);
 
-		// output
-		$modelContentMeta->readId([$_REQUEST['id']]);
-		return new Response(json_encode($modelContentMeta->getDataFirst()));
-	}
+        // output
+        return new Response(json_encode($modelContentMeta->getRowCount()));
+    }
+
+
+    public function update()
+    {
+        
+        // validate
+        if (! isset($_REQUEST['id']) || ! isset($_REQUEST['contentId']) || ! isset($_REQUEST['name']) || ! isset($_REQUEST['value'])) {
+            throw new Exception;
+        }
+
+        // resource
+        $modelContentMeta = new Model\Content\Meta;
+        $entityContentMeta = new Entity\Content\Meta;
+
+        // action
+        $entityContentMeta->consumeArray($_REQUEST);
+        $modelContentMeta->update([$entityContentMeta]);
+
+        // validate it happend
+        if (! $modelContentMeta->getRowCount()) {
+            throw new Exception;
+        }
+
+        // output
+        $modelContentMeta->readId([$_REQUEST['id']]);
+        return new Response(json_encode($modelContentMeta->getDataFirst()));
+    }
 }
